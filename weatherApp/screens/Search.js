9192690c3a4400c5ebe360526a2f30ecda5751f4
SantiCh-coder <santiagochantada@gmail.com>
2020-11-05 17:27:45 -0300

    Add a header using react-native-paper

---
+++ weatherApp/screens/Search.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Appbar, Title } from 'react-native-paper';
+import {View, Text} from 'react-native';
+import Header from './Header';
+const Search = () => {
+
+
+  return (
+    <View style={{flex:1}}>
+        <Header name="Search Screen"/>
+        <Text>Search screen</Text>
+    </View>
+  );
+};
+
+export default Search;
