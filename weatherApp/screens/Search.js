2bd242298d6621f8014ea47b94d628288f6eeb5a
SantiCh-coder <santiagochantada@gmail.com>
2020-11-05 20:36:30 -0300

    Now i can recieved data from an api and show it

--- weatherApp/screens/Search.js
+++ weatherApp/screens/Search.js
@@ -1,14 +1,45 @@
-import * as React from 'react';
+import React, {useState} from 'react';
+import { TextInput, Button, Card } from 'react-native-paper';
 import { Appbar, Title } from 'react-native-paper';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import Header from './Header';
 const Search = () => {
-
-
+  const [city, setCity] = useState('');
+  const [cities, setCities] = useState([]);
+  const fetchCities = (text) =>{
+        setCity(text)
+        fetch("https://api.weather.com/v3/location/search?apiKey=6532d6454b8aa370768e63d6ba5a832e&language=en-US&query="+text+"&locationType=city&format=json")
+        .then(item=>item.json())
+        .then(cityData=>{
+            setCities(cityData.location.address.slice(0,9))
+            console.log(cityData.location.address.slice(0,9))
+        })
+  }
   return (
     <View style={{flex:1}}>
         <Header name="Search Screen"/>
-        <Text>Search screen</Text>
+        <TextInput
+        label="city name"
+        theme={{colors:{primary:"#00aaff"}}}
+        value={city}
+        onChangeText={(text)=>fetchCities(text)}/>
+
+        <Button icon="content-save" mode="contained" onPress={() => console.log('Pressed')}
+        theme={{colors:{primary:"#00aaff"}}}
+        style={{margin:20}}>
+            <Text style={{color:"white"}}>Press me</Text>
+        </Button>
+        <FlatList 
+        data={cities}
+        renderItem={({item})=>{
+            return(
+                <Card style={{margin:2, padding:12}}>
+                    <Text>{item}</Text>
+                </Card>
+            )
+        }}
+        keyExtractor={item => item}
+        />
     </View>
   );
 };
